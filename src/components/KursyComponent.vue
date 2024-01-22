0659cdf884b01e03f6fa2828b414bc97e54a7a2b
Delta11235 <114833391+Delta11235@users.noreply.github.com>
2024-01-22 18:40:31 +0100

    Ukończono widok - kursy

---
+++ src/components/KursyComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-if="items">
+        <table>
+            <thead>
+                <tr>
+                    <td>kod</td>
+                    <td>waluta</td>
+                    <td>kurs</td>
+                </tr>
+            </thead>
+                <tr v-for="rate in items[0].rates" :key="rate.no">
+                    <td>{{ rate.code }}</td>
+                    <td>{{ rate.currency }}</td>
+                    <td>{{ rate.mid }}</td>
+                </tr>
+        </table>
+        <br>
+    </div>
+    <div v-else>loading data</div>
+</template>
+<script>
+export default {
+    name: 'KursyComponent',
+    data() {
+        return {
+            items: null
+        }
+    },
+    mounted() {
+        fetch('http://api.nbp.pl/api/exchangerates/tables/A/?format=json')
+            .then(res => res.json())
+            .then(data => this.items = data)
+            .catch(err => console.log(err.message))
+    }
+}
+</script>
+<style scoped>
+td {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    padding: 5px;
+    border: 1px solid black;
+}
+div{
+    display: flex;
+    justify-content: center;
+    text-align: center;
+}
+td:hover{
+    background-color: rgb(51, 44, 44);
+    color: rgb(207, 192, 192);
+    cursor: crosshair;
+}
+</style>
--- src/views/KursyView.vue
+++ src/views/KursyView.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="home">
         <h1>This is fetch page</h1>
-        <FetchData></FetchData>
+        <KursyComponent></KursyComponent>
     </div>
 </template>
   
 <script>
 // @ is an alias to /src
-import FetchData from "../components/FetchData.vue";
+import KursyComponent from "../components/KursyComponent.vue";
 
 
 export default {
-    name: 'HomeView',
+    name: 'KursyView',
     components: {
-        FetchData
+        KursyComponent
     }
 }
 </script>
